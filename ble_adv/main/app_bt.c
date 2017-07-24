f42d6782cdfe16e5afcea41c780a9d2e368b128d
MrDat96 <datntse62120@fpt.edu.vn>
2017-07-24 12:32:48 +0800

    Add ESP32 code flash

---
+++ ble_adv/main/app_bt.c
@@ -0,0 +1,394 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_deep_sleep.h"
+
+#include "esp_system.h"
+#include "esp_partition.h"
+#include "bt.h"
+#include "esp_log.h"
+
+#include "nvs_flash.h"
+#include "nvs.h"
+
+#include "driver/rtc_io.h"
+#include <rom/rtc.h>
+
+
+#define GPIO_INPUT_IO_TRIGGER     2
+#define GPIO_INPUT_IO_RESETCOUNT  13
+
+static const char *tag = "BLE_ADV";
+
+#define HCI_H4_CMD_PREAMBLE_SIZE           (4)
+
+/*  HCI Command opcode group field(OGF) */
+#define HCI_GRP_HOST_CONT_BASEBAND_CMDS    (0x03 << 10)            /* 0x0C00 */
+#define HCI_GRP_BLE_CMDS                   (0x08 << 10)
+
+#define HCI_RESET                          (0x0003 | HCI_GRP_HOST_CONT_BASEBAND_CMDS)
+#define HCI_BLE_WRITE_ADV_ENABLE           (0x000A | HCI_GRP_BLE_CMDS)
+#define HCI_BLE_WRITE_ADV_PARAMS           (0x0006 | HCI_GRP_BLE_CMDS)
+#define HCI_BLE_WRITE_ADV_DATA             (0x0008 | HCI_GRP_BLE_CMDS)
+
+#define HCIC_PARAM_SIZE_WRITE_ADV_ENABLE        (1)
+#define HCIC_PARAM_SIZE_BLE_WRITE_ADV_PARAMS    (15)
+#define HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA      (31)
+
+#define BD_ADDR_LEN     (6)                     /* Device address length */
+typedef uint8_t bd_addr_t[BD_ADDR_LEN];         /* Device address */
+
+#define UINT16_TO_STREAM(p, u16) {*(p)++ = (uint8_t)(u16); *(p)++ = (uint8_t)((u16) >> 8);}
+#define UINT8_TO_STREAM(p, u8)   {*(p)++ = (uint8_t)(u8);}
+#define BDADDR_TO_STREAM(p, a)   {int ijk; for (ijk = 0; ijk < BD_ADDR_LEN;  ijk++) *(p)++ = (uint8_t) a[BD_ADDR_LEN - 1 - ijk];}
+#define ARRAY_TO_STREAM(p, a, len) {int ijk; for (ijk = 0; ijk < len;        ijk++) *(p)++ = (uint8_t) a[ijk];}
+
+enum {
+    H4_TYPE_COMMAND = 1,
+    H4_TYPE_ACL     = 2,
+    H4_TYPE_SCO     = 3,
+    H4_TYPE_EVENT   = 4
+};
+
+static uint8_t hci_cmd_buf[128];
+void resetCount();
+void readCountFlash(int* serial, int* count);
+void writeDataFlash(int serial, int count);
+
+/*
+ * @brief: BT controller callback function, used to notify the upper layer that
+ *         controller is ready to receive command
+ */
+static void controller_rcv_pkt_ready(void)
+{
+    printf("controller rcv pkt ready\n");
+}
+
+void onButton(){
+    int count = 0;
+    int serial = 0;
+    readCountFlash(&serial, &count);
+    count++;
+    serial++;
+    String out = String(serial);
+    out += " " + String(count);
+    writeDataFlash(serial, count);
+    ble.begin(out);
+}
+
+
+
+void onToppedUp() {
+    int count = 0;
+    int serial = 0;
+    readCountFlash(&serial, &count);
+    if (count == 0) return;
+    resetCount();
+    count = 0;
+    serial++;
+    String out = String(serial);
+    out += " " + String(count);
+    writeDataFlash(serial, count);
+    ble.begin(out);
+}
+
+/*
+ * @brief: BT controller callback function, to transfer data packet to upper
+ *         controller is ready to receive command
+ */
+static int host_rcv_pkt(uint8_t *data, uint16_t len)
+{
+    printf("host rcv pkt: ");
+    for (uint16_t i = 0; i < len; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+static esp_vhci_host_callback_t vhci_host_cb = {
+    controller_rcv_pkt_ready,
+    host_rcv_pkt
+};
+
+static uint16_t make_cmd_reset(uint8_t *buf)
+{
+    UINT8_TO_STREAM (buf, H4_TYPE_COMMAND);
+    UINT16_TO_STREAM (buf, HCI_RESET);
+    UINT8_TO_STREAM (buf, 0);
+    return HCI_H4_CMD_PREAMBLE_SIZE;
+}
+
+static uint16_t make_cmd_ble_set_adv_enable (uint8_t *buf, uint8_t adv_enable)
+{
+    UINT8_TO_STREAM (buf, H4_TYPE_COMMAND);
+    UINT16_TO_STREAM (buf, HCI_BLE_WRITE_ADV_ENABLE);
+    UINT8_TO_STREAM  (buf, HCIC_PARAM_SIZE_WRITE_ADV_ENABLE);
+    UINT8_TO_STREAM (buf, adv_enable);
+    return HCI_H4_CMD_PREAMBLE_SIZE + HCIC_PARAM_SIZE_WRITE_ADV_ENABLE;
+}
+
+static uint16_t make_cmd_ble_set_adv_param (uint8_t *buf, uint16_t adv_int_min, uint16_t adv_int_max,
+        uint8_t adv_type, uint8_t addr_type_own,
+        uint8_t addr_type_dir, bd_addr_t direct_bda,
+        uint8_t channel_map, uint8_t adv_filter_policy)
+{
+    UINT8_TO_STREAM (buf, H4_TYPE_COMMAND);
+    UINT16_TO_STREAM (buf, HCI_BLE_WRITE_ADV_PARAMS);
+    UINT8_TO_STREAM  (buf, HCIC_PARAM_SIZE_BLE_WRITE_ADV_PARAMS );
+
+    UINT16_TO_STREAM (buf, adv_int_min);
+    UINT16_TO_STREAM (buf, adv_int_max);
+    UINT8_TO_STREAM (buf, adv_type);
+    UINT8_TO_STREAM (buf, addr_type_own);
+    UINT8_TO_STREAM (buf, addr_type_dir);
+    BDADDR_TO_STREAM (buf, direct_bda);
+    UINT8_TO_STREAM (buf, channel_map);
+    UINT8_TO_STREAM (buf, adv_filter_policy);
+    return HCI_H4_CMD_PREAMBLE_SIZE + HCIC_PARAM_SIZE_BLE_WRITE_ADV_PARAMS;
+}
+
+
+static uint16_t make_cmd_ble_set_adv_data(uint8_t *buf, uint8_t data_len, uint8_t *p_data)
+{
+    UINT8_TO_STREAM (buf, H4_TYPE_COMMAND);
+    UINT16_TO_STREAM (buf, HCI_BLE_WRITE_ADV_DATA);
+    UINT8_TO_STREAM  (buf, HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA + 1);
+
+    memset(buf, 0, HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA);
+
+    if (p_data != NULL && data_len > 0) {
+        if (data_len > HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA) {
+            data_len = HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA;
+        }
+
+        UINT8_TO_STREAM (buf, data_len);
+
+        ARRAY_TO_STREAM (buf, p_data, data_len);
+    }
+    return HCI_H4_CMD_PREAMBLE_SIZE + HCIC_PARAM_SIZE_BLE_WRITE_ADV_DATA + 1;
+}
+
+static void hci_cmd_send_reset(void)
+{
+    uint16_t sz = make_cmd_reset (hci_cmd_buf);
+    esp_vhci_host_send_packet(hci_cmd_buf, sz);
+}
+
+static void hci_cmd_send_ble_adv_start(void)
+{
+    uint16_t sz = make_cmd_ble_set_adv_enable (hci_cmd_buf, 1);
+    esp_vhci_host_send_packet(hci_cmd_buf, sz);
+}
+
+static void hci_cmd_send_ble_set_adv_param(void)
+{
+    uint16_t adv_intv_min = 256; // 160ms
+    uint16_t adv_intv_max = 256; // 160ms
+    uint8_t adv_type = 0; // connectable undirected advertising (ADV_IND)
+    uint8_t own_addr_type = 0; // Public Device Address
+    uint8_t peer_addr_type = 0; // Public Device Address
+    uint8_t peer_addr[6] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85};
+    uint8_t adv_chn_map = 0x07; // 37, 38, 39
+    uint8_t adv_filter_policy = 0; // Process All Conn and Scan
+
+    uint16_t sz = make_cmd_ble_set_adv_param(hci_cmd_buf,
+                  adv_intv_min,
+                  adv_intv_max,
+                  adv_type,
+                  own_addr_type,
+                  peer_addr_type,
+                  peer_addr,
+                  adv_chn_map,
+                  adv_filter_policy);
+    esp_vhci_host_send_packet(hci_cmd_buf, sz);
+}
+
+static void hci_cmd_send_ble_set_adv_data(char *adv_name)
+{
+    //char *adv_name = "ESP-BLE-HELLO";
+    uint8_t name_len = (uint8_t)strlen(adv_name);
+    uint8_t adv_data[31] = {0x02, 0x01, 0x06, 0x0, 0x09};
+    uint8_t adv_data_len;
+
+    adv_data[3] = name_len + 1;
+    for (int i = 0; i < name_len; i++) {
+        adv_data[5 + i] = (uint8_t)adv_name[i];
+    }
+    adv_data_len = 5 + name_len;
+
+    uint16_t sz = make_cmd_ble_set_adv_data(hci_cmd_buf, adv_data_len, (uint8_t *)adv_data);
+    esp_vhci_host_send_packet(hci_cmd_buf, sz);
+}
+
+/*
+ * @brief: send HCI commands to perform BLE advertising;
+ */
+void bleAdvtTask(void *pvParameters)
+{
+    int cmd_cnt = 0;
+    bool send_avail = false;
+    esp_vhci_host_register_callback(&vhci_host_cb);
+    printf("BLE advt task start\n");
+	int timeAdv = 0;
+	char *adv_data = "MRDAT_SAY_HELLO";
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        send_avail = esp_vhci_host_check_send_available();
+        if (send_avail) {
+            switch (cmd_cnt) {
+            case 0: hci_cmd_send_reset(); ++cmd_cnt; break;
+            case 1: hci_cmd_send_ble_set_adv_param(); ++cmd_cnt; break;
+            case 2: hci_cmd_send_ble_set_adv_data(adv_data); ++cmd_cnt; break;
+            case 3: hci_cmd_send_ble_adv_start(); ++cmd_cnt; break;
+            }
+        }
+        printf("BLE Advertise, flag_send_avail: %d, cmd_sent: %d\n", send_avail, cmd_cnt);
+		timeAdv++;
+		if (timeAdv == 5)
+		esp_deep_sleep_start();
+    }
+}
+
+
+void wakeupCause() {
+  switch (esp_deep_sleep_get_wakeup_cause()) {
+        case ESP_DEEP_SLEEP_WAKEUP_EXT1: {
+            uint64_t wakeup_pin_mask = esp_deep_sleep_get_ext1_wakeup_status();
+            if (wakeup_pin_mask != 0) {
+                int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
+                printf("Wake up from GPIO %d\n", pin);
+                if (pin == GPIO_INPUT_IO_TRIGGER) {
+                    //onButton();
+					printf("Wake up from Trigger\n");
+                } else if (pin == GPIO_INPUT_IO_RESETCOUNT) {
+                    //onToppedUp();
+					printf("Wake up from reset count\n");
+                }
+            } else {
+                printf("Wake up from GPIO\n");
+            }
+            break;
+        }
+        default:
+          printf("Wake up from RESET");
+          //ble.begin("RESET");
+  }
+}
+
+
+void setupPins() {
+    esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
+    const int ext_wakeup_pin_1 = GPIO_INPUT_IO_TRIGGER;
+    const int ext_wakeup_pin_2 = GPIO_INPUT_IO_RESETCOUNT;
+    //pinMode(GPIO_INPUT_IO_TRIGGER, INPUT_PULLDOWN);
+    //pinMode(GPIO_INPUT_IO_RESETCOUNT, INPUT_PULLDOWN);
+
+    const uint64_t ext_wakeup_pin_1_mask = 1LL << ext_wakeup_pin_1;
+    const uint64_t ext_wakeup_pin_2_mask = 1LL << ext_wakeup_pin_2;
+    esp_deep_sleep_enable_ext1_wakeup(ext_wakeup_pin_1_mask | ext_wakeup_pin_2_mask, ESP_EXT1_WAKEUP_ANY_HIGH);
+    
+}
+
+void app_main()
+{
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    wakeupCause();
+    setupPins();
+    if (esp_bt_controller_init(&bt_cfg) != ESP_OK) {
+        ESP_LOGI(tag, "Bluetooth controller initialize failed");
+        return;
+    }
+
+    if (esp_bt_controller_enable(ESP_BT_MODE_BTDM) != ESP_OK) {
+        ESP_LOGI(tag, "Bluetooth controller enable failed");
+        return;
+    }
+
+    xTaskCreatePinnedToCore(&bleAdvtTask, "bleAdvtTask", 2048, NULL, 5, NULL, 0);
+}
+
+void readCountFlash(int* serial, int* count) {
+    esp_err_t err = nvs_flash_init();
+     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
+        const esp_partition_t* nvs_partition = esp_partition_find_first(
+                ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
+        ESP_ERROR_CHECK( esp_partition_erase_range(nvs_partition, 0, nvs_partition->size) );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+
+    nvs_handle my_handle;
+    err = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err != ESP_OK) {
+        printf("Error (%d) opening NVS handle!\n", err);
+    } else {
+      
+        err = nvs_get_i32(my_handle, "serial", serial);
+        
+        switch (err) {
+            case ESP_OK:
+                //printf("Read Serial successfull\n");
+                break;
+            case ESP_ERR_NVS_NOT_FOUND:
+                printf("The value is not initialized yet!\n");
+                break;
+            default :
+                printf("Error (%d) reading!\n", err);
+        }
+        err = nvs_get_i32(my_handle, "counter", count);
+        switch (err) {
+            case ESP_OK:
+                //printf("Read Counter successfull\n");
+                break;
+            case ESP_ERR_NVS_NOT_FOUND:
+                printf("The value is not initialized yet!\n");
+                break;
+            default :
+                printf("Error (%d) reading!\n", err);
+        }
+        nvs_close(my_handle);
+    }
+}
+
+void writeDataFlash(int serial, int count) {
+    esp_err_t err = nvs_flash_init();
+     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
+        const esp_partition_t* nvs_partition = esp_partition_find_first(
+                ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
+        ESP_ERROR_CHECK( esp_partition_erase_range(nvs_partition, 0, nvs_partition->size) );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+    nvs_handle my_handle;
+    err = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err != ESP_OK) {
+        printf("Error (%d) opening NVS handle!\n", err);
+    } else {
+        err = nvs_set_i32(my_handle, "serial", serial);
+        err = nvs_set_i32(my_handle, "counter", count);
+        err = nvs_commit(my_handle);
+        nvs_close(my_handle);
+    }
+}
+//set counter to 0
+void resetCount() {
+    esp_err_t err = nvs_flash_init();
+     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
+        const esp_partition_t* nvs_partition = esp_partition_find_first(
+                ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_NVS, NULL);
+        ESP_ERROR_CHECK( esp_partition_erase_range(nvs_partition, 0, nvs_partition->size) );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( err );
+    nvs_handle my_handle;
+    err = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err != ESP_OK) {
+        printf("Error (%d) opening NVS handle!\n", err);
+    } else {
+        err = nvs_set_i32(my_handle, "counter", 0);
+        err = nvs_commit(my_handle);
+        nvs_close(my_handle);
+    }
+}
